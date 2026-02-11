4dc8bd0316437cb8afe2db4788ab1361c4fd35a0
sadsalat <74601305+sadsalat@users.noreply.github.com>
2026-02-11 14:40:22 +0300

    rebel refuge armor fix ragdoll perdiction adjustments walkie-talkie edits, hud removed added screen on model

--- lua/homigrad/playerclass/classes/sh_rebel.lua
+++ lua/homigrad/playerclass/classes/sh_rebel.lua
@@ -199,12 +199,11 @@ local function giveSubClassLoadout(ply, subClass)
     local randFace = face_list[math.random(#face_list)]
     local randHelmet = helmet_list[math.random(#helmet_list)]
 
-    if randVest ~= "" then ply.armors["torso"] = randVest end
-    if randHelmet ~= "" then ply.armors["head"] = randHelmet end
-    if randFace ~= "" then ply.armors["face"] = randFace end
+    if randVest ~= "" then hg.AddArmor(ply, randVest) end
+    if randHelmet ~= "" then hg.AddArmor(ply, randHelmet) end
+    if randFace ~= "" then hg.AddArmor(ply, randFace) end
 
     ply:SyncArmor()
-
 
     ply:Give("weapon_melee")
     ply:Give("weapon_walkie_talkie")
